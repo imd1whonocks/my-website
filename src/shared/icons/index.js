452e0cd7a8d126adc8bbf3835fbcd4a2f5511eb9
Shashank <shashank2410@gmail.com>
2020-06-29 01:27:13 +0530

    header web done

---
+++ src/shared/icons/index.js
@@ -0,0 +1,12 @@
+import MailIcon from './MailIcon';
+import PhoneIcon from './PhoneIcon';
+import LinkedinIcon from './LinkedinIcon';
+import GithubIcon from './GithubIcon';
+
+
+module.exports = {
+    MailIcon,
+    PhoneIcon,
+    LinkedinIcon,
+    GithubIcon,
+}
